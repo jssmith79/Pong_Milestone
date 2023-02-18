7dd6ee791d624010ab6dd47a5797310c535bf65e
jssmith79 <i.am.hef@gmail.com>
2023-02-18 13:05:58 -0800

    completed JS

--- action.js
+++ action.js
@@ -1,3 +1,5 @@
+//defining all the variables to make the game work
+
 let gameState = 'Start';
 let paddle_1 = document.querySelector('.paddle_1');
 let paddle_2 = document.querySelector('.paddle_2');
@@ -18,6 +20,8 @@ let dx = Math.floor(Math.random() * 4) + 3;
 let dy = Math.floor(Math.random() * 4 ) + 3;
 let dxd = Math.floor(Math.random() * 2);
 let dyd = Math.floor(Math.random() * 2);
+
+//these event listeners make the game start
 
 document.addEventListener('keydown', (e) => {
     if (e.key == 'Enter') {
@@ -70,5 +74,55 @@ if (gameState == 'play') {
     paddle_2_coord = paddle_2.getBoundingClientRect();
     }
  }
+//makes the ball move across the screen.  The Math.random function decides where the ball will move
+ function moveBall(dx, dy, dxd, dyd) {
+    if (ball_coord.top <= board_coord.top){
+        dyd = 1;
+    }
+    if (ball_coord.bottom >= board_coord.bottom){
+        dyd = 0;
+    }
+    if (
+        ball_coord.left <= paddle_1_coord.right &&
+        ball_coord.top >= paddle_1_coord.top &&
+        ball_coord.bottom <= paddle_1_coord.bottom 
+    ) {
+        dxd = 1;
+        dx = Math.floor(Math.random() * 4) + 3;
+        dy = Math.floor(Math.random() * 4) + 3;
+    }
+    if (
+        ball_coord.right >= paddle_2_coord.left &&
+        ball_coord.top >= paddle_2_coord.top &&
+        ball_coord.bottom <= paddle_2_coord.bottom
+    )  {
+        dxd = 0
+        dx = Math.floor(Math.random() * 4) + 3;
+        dy = Math.floor(Math.random() * 4) + 3;
+    }
+//This is how the scoring is compiled
+    if (
+        ball_coord.left <= board_coord.left ||
+        ball_coord.right >= board_coord.right
+    ) {
+        if (ball_coord.left <= board_coord.left) {
+            score_2.innerHTML = +score_2.innerHTML +1;
+        } else {
+            score_1.innerHTML = +score_1.innerHTML +1;
+        }
+        gameState = 'start';
 
- 
+        ball_coord = initial_ball_coord;
+        ball.style = initial_ball.style;
+        message.innerHTML = 'Press enter to Play'
+        message.style .left = + 'vw';
+        return;
+    }
+
+    ball.style.top = ball_coord.top + dy * (dyd == 0 ? -1 : 1) + 'px';
+    ball.style.left = ball_coord.left + dx * (dxd == 0 ? -1 : 1) + 'px';
+    ball_coord = ball.getBoundingClientRect();
+    requestAnimationFrame(() => {
+    moveBall(dx, dy, dxd, dyd);
+});
+ }
